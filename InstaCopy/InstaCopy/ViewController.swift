7d2ae5b1cfe0a643238e1e51d0e55280d511debd
itisioslab <itisioslab@Mac-mini-administrator-2.local>
2018-09-29 13:15:12 +0300

    segmentrd

--- InstaCopy/InstaCopy/ViewController.swift
+++ InstaCopy/InstaCopy/ViewController.swift
@@ -9,17 +9,128 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var containerView: UIView!
+    
+    private lazy var firstViewController: FirstViewController = {
+        // Load Storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        // Instantiate View Controller
+        var viewController = storyboard.instantiateViewController(withIdentifier: "FirstViewController") as! FirstViewController
+        
+        // Add View Controller as Child View Controller
+        self.add(asChildViewController: viewController)
+        
+        return viewController
+    }()
+    
+    private lazy var secondViewController: SecondViewController = {
+        // Load Storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        // Instantiate View Controller
+        var viewController = storyboard.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        
+        // Add View Controller as Child View Controller
+        self.add(asChildViewController: viewController)
+        
+        return viewController
+    }()
+    
+    private lazy var thirdViewController: ThirdViewController = {
+        // Load Storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        // Instantiate View Controller
+        var viewController = storyboard.instantiateViewController(withIdentifier: "ThirdViewController") as! ThirdViewController
+        
+        // Add View Controller as Child View Controller
+        self.add(asChildViewController: viewController)
+        
+        return viewController
+    }()
+    
+    static func viewController() -> ViewController {
+        return UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController") as! ViewController
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func segmentValueChanged(_ sender: UISegmentedControl) {
+        updateView()
+    }
+    
+    
+    private func add(asChildViewController viewController: UIViewController) {
+        
+        // Add Child View Controller
+        addChildViewController(viewController)
+        
+        // Add Child View as Subview
+        containerView.addSubview(viewController.view)
+        
+        // Configure Child View
+        viewController.view.frame = containerView.bounds
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        // Notify Child View Controller
+        viewController.didMove(toParentViewController: self)
+    }
+    
+    private func remove(asChildViewController viewController: UIViewController) {
+        // Notify Child View Controller
+        viewController.willMove(toParentViewController: nil)
+        
+        // Remove Child View From Superview
+        viewController.view.removeFromSuperview()
+        
+        // Notify Child View Controller
+        viewController.removeFromParentViewController()
+    }
+    
+    
+    private func updateView() {
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+            remove(asChildViewController: secondViewController)
+            remove(asChildViewController: thirdViewController)
+            add(asChildViewController: firstViewController)
+        case 1:
+            remove(asChildViewController: thirdViewController)
+            remove(asChildViewController: firstViewController)
+            add(asChildViewController: secondViewController)
+        case 2:
+            remove(asChildViewController: secondViewController)
+            remove(asChildViewController: firstViewController)
+            add(asChildViewController: thirdViewController)
+        default:
+            break
+        }
+    }
+    
+    func setupView() {
+        updateView()
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
 }
 
